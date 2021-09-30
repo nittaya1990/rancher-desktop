00c4d6f20313d479118d909c11b4d6ac085a2c01
Eric Promislow <epromislow@suse.com>
2021-09-29 19:13:52 -0700

    Add a nerdctl-based front-end with choice of namespace
    - The namespace is stored in prefs: .images.namespace
    - Currently it's hardwired to using nerdctl, but all the kim code is there.
    
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- src/main/kim.ts
+++ src/main/kim.ts
@@ -20,7 +20,7 @@ let imageManager: ImageProcessor;
 let lastBuildDirectory = '';
 let mountCount = 0;
 
-export function setupImageProcessor(imageProcessorName: string, k8sManager: K8s.KubernetesBackend) {
+export function setupImageProcessor(imageProcessorName: string, k8sManager: K8s.KubernetesBackend): ImageProcessor {
   imageManager = imageManager ?? createImageProcessor(imageProcessorName, k8sManager);
 
   interface ImageContents {
@@ -178,4 +178,6 @@ export function setupImageProcessor(imageProcessorName: string, k8sManager: K8s.
   Electron.ipcMain.handle('images-check-state', () => {
     return imageManager.isReady;
   });
+
+  return imageManager;
 }
